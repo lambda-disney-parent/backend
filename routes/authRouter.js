8f2aa50d1049f05b1616b625b7d2b64cfca9d8e3
blent1050 <blent1050@gmail.com>
2019-03-12 13:15:44 -0400

    added a check to see if user exists when registering

--- routes/authRouter.js
+++ routes/authRouter.js
@@ -7,14 +7,22 @@ const generateToken = require("../Auth/token-service");
 const Users = require("../helpers/user-model");
 
 //Routes
-router.post("/register", (req, res) => {
+router.post("/register", async (req, res) => {
   let user = req.body;
   const hash = bcrypt.hashSync(user.password, 10);
-
   user.password = hash;
+
+  //checks if user is created
+  const { username } = req.body;
+  if (await Users.findBy({ username }).first()) {
+    res.status(403).json({ message: "This account already exists!" });
+  }
+
   Users.add(user)
     .then(saved => {
-      res.status(201).json(saved);
+      res
+        .status(201)
+        .json({ message: "Account created!", username: saved.username });
     })
     .catch(err => res.status(500).json(err));
 });
@@ -27,8 +35,7 @@ router.post("/login", (req, res) => {
         const token = generateToken(user);
         res.status(200).json({
           message: "Welcome!",
-          token,
-          roles: token.roles
+          token
         });
       } else {
         res.status(401).json({ message: "Invalid Credentials" });
